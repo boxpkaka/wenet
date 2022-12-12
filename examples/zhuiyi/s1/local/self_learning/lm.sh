735266d98e1d35ce986f5d5f57167b3b750938a4
fangcheng1050 <fangcheng1050@wezhuiyi.com>
2022-12-12 02:21:42 +0000

    支持gpu推理自学习平台lm.bin文件生成

--- examples/zhuiyi/s1/local/self_learning/lm.sh
+++ examples/zhuiyi/s1/local/self_learning/lm.sh
@@ -13,7 +13,9 @@ average_checkpoint=true
 average_num=5
 
 order=3
-lambda=0.85
+lambda=0.6
+is_gpu_infer=false
+is_kn_smooth=false
 
 . tools/parse_options.sh || exit 1
 
@@ -23,7 +25,9 @@ if [ $# != 3 ]; then
   echo "text: 调优需要的文本."
   echo "out_dir: 输出文件夹."
   echo "--order: 默认3."
-  echo "--lambda: 默认0.85."
+  echo "--lambda: 默认0.6."
+  echo "--is_gpu_infer: 是否为gpu推理需要的lm文件, 默认否."
+  echo "--is_kn_smooth: 是否为kneserney平滑算法, 默认否, 即使用wittenbell平滑."
   exit 1
 fi
 
@@ -34,20 +38,27 @@ out_dir=$3
 data=$self_learning/data/
 
 lm=$out_dir/data/local/lm
+
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
   echo "$(date) stage 0: train lm."
   mkdir -p $lm
   cp $text $lm/text
-  local/train_lms.sh $lm/text $data/local/dict/lexicon.txt $lm
+  local/train_lms.sh --is_kn_smooth ${is_kn_smooth} $lm/text $data/local/dict/lexicon.txt $lm
   mv ${lm}/lm.arpa ${lm}/part.arpa
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   echo "$(date) stage 1: interpolate ngrams."
-  ngram -lm $data/local/lm/lm.arpa -order ${order} -mix-lm ${lm}/part.arpa -lambda ${lambda} -write-lm ${lm}/lm.arpa
+    ngram -lm $data/local/lm/lm.arpa -order ${order} -mix-lm ${lm}/part.arpa -lambda ${lambda} -write-lm ${lm}/lm.arpa
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-  echo "$(date) stage 2: Build decoding TLG."
-  tools/fst/make_tlg.sh ${lm} $data/local/lang $out_dir/data/lang_test || exit 1
+  if [[ ${is_gpu_infer} == true ]];
+  then
+    echo "$(date) stage 2: Build lm.bin."
+    build_binary -s ${lm}/lm.arpa $out_dir/lm.bin || exit 1
+  else
+    echo "$(date) stage 2: Build decoding TLG."
+    tools/fst/make_tlg.sh ${lm} $data/local/lang $out_dir/data/lang_test || exit 1
+  fi
 fi
